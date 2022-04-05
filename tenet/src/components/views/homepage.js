0db5433f1ee755275d60fdd0db46f966dd83228b
Niloo-r <nrahmani@kth.se>
2022-04-05 16:08:55 +0200

    test

--- tenet/src/components/views/homepage.js
+++ tenet/src/components/views/homepage.js
@@ -1,16 +1,9 @@
-
-
-
 function Homepage() {
-  return (
-    <div className="homepage">
-     
-        
-          Nils
-        
-    
-    </div>
-  );
+	return (
+		<div className="homepage">
+			<h2>Decibel</h2>
+		</div>
+	);
 }
 
 export default Homepage;
