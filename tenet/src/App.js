a14d0c2fc9584bd568e0afef34d772bbf4306e4c
jkotieno <jkotieno@kth.se>
2022-04-18 05:52:20 +0200

    added files, and firebase config

--- tenet/src/App.js
+++ tenet/src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import db from './components/config/firebase';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [name , setName] = useState();
+  const [age , setAge] = useState();
+      
+  // Push Function
+  const Push = () => {
+    db.ref("user").set({
+      name : name,
+      age : age,
+    }).catch(alert);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Group 11:  Work in progress.... 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginTop : 250}}>
+      <center>
+      <input placeholder="Enter your name" value={name} 
+      onChange={(e) => setName(e.target.value)}/>
+      <br/><br/>
+      <input placeholder="Enter your age" value={age} 
+      onChange={(e) => setAge(e.target.value)}/>
+      <br/><br/> 
+      <button onClick={Push}>PUSH</button>
+      </center>
     </div>
   );
 }
-
+  
 export default App;
