cb1958bc561a0c9d71d8db9162727a56904558eb
nariman haidar <narimanh@kth.se>
2022-04-26 13:59:50 +0200

    narin commits

--- tenet/src/App.js
+++ tenet/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
@@ -19,7 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
+
   );
 }
 
