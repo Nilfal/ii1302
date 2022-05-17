4a2a7c1361bd6348522345ca723dd6b412d7509e
alvamariaols <96184834+alvamariaols@users.noreply.github.com>
2022-05-17 14:17:10 +0200

    added things again

--- tenet/src/components/views/informationpage.js
+++ tenet/src/components/views/informationpage.js
@@ -5,7 +5,45 @@ import '../css/informationpage.css';
 function Information() {
 	return (
 		<div className='info'>
-			<p> haha </p>
+			<a href={"https://www.kth.se/student/kurser/kurs/II1302"}><div>
+				<p>Allt i denna kurs bakas in i projektet där arbetstiden är 180h-200h per student (nominell kurstid) eller mer om man vill och behöver.
+
+Eklund (projektboken från ingenjörsmetodiken) definierar följande mål i ett projekt:</p>
+			<ul>
+				<li>
+				Effektmål 
+				<ul><li>
+				Att "förbättra" eller "förändra" något med hjälp av IT-teknik.
+					</li></ul>
+				</li>
+				<li>
+				Resultatmål 
+				<ul><li>
+				Skapa en IT-produkt eller tjänst som åstadkommer denna förbättring.
+				</li><li>
+				Leverans är fungerande prototyp med tillhörande teknisk dokumentation på GitHub.
+				</li>
+				</ul>
+				<li>
+				Projektmål
+				<ul>
+					<li>
+					Att som student i en projektgrupp med andra studenter lära sig om projektmetoder för IT-projekt och klara kursen II1302 med bra betyg.
+					</li>
+					<li>
+					Skriva en slutrapport som försöker svara på frågan "Vad är en bra projektmetod för små IT-projekt"
+						</li></ul>	
+				</li>
+				
+				</li>
+			</ul>
+			</div></a>
+			<div>
+				<p>2</p>
+			</div>
+			<div>
+				<p>3</p>
+			</div>
 		</div>
 	);
 }
